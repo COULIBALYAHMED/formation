521bd8dfbc2b31a633da5527fc3418802631e4fe
Ahmed <coulibaly_ahmed@hotmail.com>
2016-10-28 17:56:34 +0200

    formation

---
+++ TPS/TPGIT/appiTest/src/main/java/appliAhmed/appiTest/App.java
@@ -0,0 +1,13 @@
+package appliAhmed.appiTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
